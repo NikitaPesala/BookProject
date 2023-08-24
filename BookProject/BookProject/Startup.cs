abd46d84f235f1a069e7c1f50a1aca52ae57591b
Lakshitha M <lakshitha.m@ecolab.com>
2023-08-24 18:42:01 +0530

    EF BookProject File changes

--- BookProject/BookProject/Startup.cs
+++ BookProject/BookProject/Startup.cs
@@ -1,37 +1,66 @@
 ﻿using ConceptArchitect.BookManagement;
 
 using BookProject.Extensions;
+using BooksWeb02;
 
 namespace BookProject
 {
-	public static class Startup
-	{
-		public static IServiceCollection ConfigureServices(this IServiceCollection services)
-		{
+    public static class Startup
+    {
+        public static IServiceCollection ConfigureServices(this IServiceCollection services)
+        {
 
-			services.AddControllersWithViews();
+            services.AddControllersWithViews();
 
-			services.AddAdoBMSRepository();
+            services.AddAdoBMSRepository();
 
-			services.AddSingleton<IAuthorService, PersistentAuthorService>();
+            services.AddTransient<IAuthorService, PersistentAuthorService>();
 
-			services.AddSingleton<IBookService, PersistentBookService>();
+            services.AddTransient<IBookService, PersistentBookService>();
 
-			return services;
-		}
+            return services;
+        }
 
-		public static IApplicationBuilder ConfigureMiddlewares(this WebApplication app)
-		{
-			if (!app.Environment.IsDevelopment())
-			{
-				app.UseExceptionHandler("/Home/Error");
-			}
-			app.UseStaticFiles();
+        public static IApplicationBuilder ConfigureMiddlewares(this WebApplication app)
+        {
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+            app.UseOnUrl("/admin/seed", async context =>
+            {
+                var authorService = context.RequestServices.GetService<IAuthorService>();
 
-			app.UseRouting();
+                await authorService.AddAuthor(new Author()
+                {
+                    Id = "dinkar",
+                    Name = "Ramdhari Singh Dinkar",
+                    Biography = "The National Poet of India",
+                    Photo = "https://pbs.twimg.com/profile_images/1269658848777785345/2bY35KV0_400x400.jpg",
+                    Tags = "poet, historian",
+                    BirthDate = new DateTime(1906, 1, 1),
+                    DeathDate = new DateTime(1976, 1, 1)
+                });
+                await authorService.AddAuthor(new Author()
+                {
+                    Id = "mahatma-gandhi",
+                    Name = "Mahamta Gandhi",
+                    Biography = "The Father of the Nation for India",
+                    Photo = "https://pbs.twimg.com/media/FAqPzrrUYAM8pCu.jpg",
+                    BirthDate = new DateTime(1869, 10, 2),
+                    Tags = "freedom fighter, social reformer",
+                    DeathDate = new DateTime(1948, 1, 30)
+                });
 
-			app.UseAuthorization();
-          
+            });
+
+
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
 
             app.MapControllerRoute(
                name: "default",
@@ -39,19 +68,21 @@ namespace BookProject
                defaults: new { controller = "Author", action = "Index" }
             );
 
-           
-            app.MapControllerRoute(
-				name: "default",
-				pattern: "{controller=Home}/{action=Index}/{id?}");
 
-			app.MapControllerRoute(
-		   name: "Add",
-		   pattern: "author/add",
-		   defaults: new { controller = "Author", action = "Add" }
-		   );
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+
+            app.MapControllerRoute(
+           name: "Add",
+           pattern: "author/add",
+           defaults: new { controller = "Author", action = "Add" }
+           );
 
             return app;
-		}
-
-	}
+        }
+    }
 }
+
+
+
---
+++ BookProject/ConceptArchitect.BookManagement/User.cs
@@ -0,0 +1,24 @@
+﻿using DataAnnotationsExtensions;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConceptArchitect.BookManagement
+{
+    [Table("Members")]
+    public class User
+    {
+        [Email]
+        [Key]
+        public string Email { get; set; }
+        public string Name { get; set; }
+        public string Password { get; set; }
+
+        [Column("PhotoUrl")]
+        public string Photo { get; set; }
+    }
+}
